ea98a9b6c713b3f5ab7f7dab8141c4ca9e27ad71
zallin <zallin93@gmail.com>
2024-10-26 00:26:56 -0400

    go fmt over the directory.

--- variables.go
+++ variables.go
@@ -2,25 +2,25 @@ package main
 
 import "fmt"
 
-func main()  {
-  var a = "initial"
-  fmt.Println(a)
+func main() {
+	var a = "initial"
+	fmt.Println(a)
 
-  // var declares one or more variables.
-  var b, c int = 1,2
-  fmt.Println(b,c)
+	// var declares one or more variables.
+	var b, c int = 1, 2
+	fmt.Println(b, c)
 
-  // go will infer the type when the variable is initialized.
-  var d = true
-  fmt.Println(d)
+	// go will infer the type when the variable is initialized.
+	var d = true
+	fmt.Println(d)
 
-  // zero-valued by default.
-  var e int
-  fmt.Println(e)
+	// zero-valued by default.
+	var e int
+	fmt.Println(e)
 
-  var g string
-  fmt.Println(g)
+	var g string
+	fmt.Println(g)
 
-  f := "apple"
-  fmt.Println(f)
+	f := "apple"
+	fmt.Println(f)
 }
