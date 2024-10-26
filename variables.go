334d24905da951590e0109eb5151915b0c9fb55a
zallin <zallin93@gmail.com>
2024-10-25 23:09:45 -0400

    Init: Initial commit while doing the Go learning tutorial. Using this to test the setup of vim-go, and other tools.

---
+++ variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main()  {
+  var a = "initial"
+  fmt.Println(a)
+
+  // var declares one or more variables.
+  var b, c int = 1,2
+  fmt.Println(b,c)
+
+  // go will infer the type when the variable is initialized.
+  var d = true
+  fmt.Println(d)
+
+  // zero-valued by default.
+  var e int
+  fmt.Println(e)
+
+  var g string
+  fmt.Println(g)
+
+  f := "apple"
+  fmt.Println(f)
+}
